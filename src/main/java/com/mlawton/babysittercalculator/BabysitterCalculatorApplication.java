17162306e2714c7a250e27b30139f2bb955f4ad0
Madison Lawton <madisonlawton@dswinc.com>
2020-10-18 16:06:37 -0400

    Initial commit after git init

---
+++ src/main/java/com/mlawton/babysittercalculator/BabysitterCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.mlawton.babysittercalculator;
+
+import java.util.Scanner;
+
+public class BabysitterCalculatorApplication {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+	}
+
+}
